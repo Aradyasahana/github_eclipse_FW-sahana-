ac3fdbcce7df5b5bf2af37d6d4743200d7fd56da
Admin <Admin@DESKTOP-OI8FCKA>
2023-05-29 15:34:39 +0530

    4567

--- Selenium/src/Selenium_script/L_byusing_classname.java
+++ Selenium/src/Selenium_script/L_byusing_classname.java
@@ -15,7 +15,8 @@ public class L_byusing_classname
 		driver.findElement(By.id("a1")).click();
 		driver.findElement(By.className("c1")).sendKeys("sahana");
 		//driver.findElement(By.className("c1")).sendKeys("sahana");
- 
+		//driver.findElement(By.className("c1")).sendKeys("sahana");
+
 	}
 
 }
